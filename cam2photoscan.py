4f4b10f60228bdd1808cfbcee8f27c6f7c456271
Aaron Curtis <foobarbecue@gmail.com>
2018-02-19 13:09:48 -0800

    Add calculation of Photoscan xml transformation matrix

--- cam2photoscan.py
+++ cam2photoscan.py
@@ -6,6 +6,8 @@ import pvl
 from clize import run
 from os import path
 from glob import glob
+import transforms3d
+import numpy
 
 ps_cam_xml_template = """<?xml version="1.0" encoding="UTF-8"?>
 <document version="1.3.0">
@@ -26,34 +28,50 @@ ps_cam_xml_template = """<?xml version="1.0" encoding="UTF-8"?>
 </document>
     """
 
-def camera_xml_snippet(position):
+def camera_xml_snippet(id_, ground_point):
+    print(id_)
     cam_template = """
     <camera id="{}" label="{}" sensor_id="0" enabled="1">
-        <!-- <transform>0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1</transform> -->
+        <transform>{}</transform>
         <orientation>1</orientation>
         <reference x="{}" y="{}" z="{}" enabled="1"/>
     </camera>
     """
-    return cam_template.format(*position)
+    # Label from filename
+    label = path.split(ground_point['Filename'])[-1] + '.tif',
 
-def dir2photoscan_cameras(*, from_dir, lat, lon, to_file):
-    positions = []
-    for id, cam_file in enumerate(glob(from_dir + '*.cub')):
+    # Calculate Photoscan-compatible rotation matrix from look direction and body fixed position
+    rot_mat = transforms3d.euler.euler2mat(*ground_point['LookDirectionBodyFixed'].value)
+    translation_col = numpy.array(ground_point['BodyFixedCoordinate'].value).reshape(3,1)
+    bottom_row = numpy.array([0,0,0,1])
+    ps_transform = numpy.hstack([rot_mat, translation_col])
+    ps_transform = numpy.vstack([ps_transform, bottom_row])
+    translation_str = ' '.join([str(x) for x in ps_transform.reshape(1,16).tolist()[0]])
+
+    # Format lat lon alt info for Photoscan reference tag
+    lat_lon_alt = [ground_point['SubSpacecraftLongitude'].value,
+                   ground_point['SubSpacecraftLatitude'].value,
+                   ground_point['SpacecraftAltitude'].value]
+    return cam_template.format(id_, label, translation_str, *lat_lon_alt)
+
+def dir2photoscan_cameras(*, from_dir, lat, lon, to_file=None):
+    ground_points = []
+    for cam_file in glob(from_dir + '*.cub'):
         try:
-            res = pvl.loads(campt(from_=path.join(from_dir, cam_file), type="ground", latitude=lat, longitude=lon))
+            ground_point = pvl.loads(
+                campt(from_=path.join(from_dir, cam_file), type="ground", latitude=lat, longitude=lon))['GroundPoint']
             #positions.append([cam_file, res['GroundPoint']['SpacecraftPosition']])
-            positions.append([id,
-                              path.split(cam_file)[-1]+'.tif',
-                              res['GroundPoint']['SubSpacecraftLongitude'][0],
-                              res['GroundPoint']['SubSpacecraftLatitude'][0],
-                              res['GroundPoint']['SpacecraftAltitude'][0]
-                              ])
+            ground_points.append(ground_point)
         except ProcessError as e:
             print(e.stderr)
+    cameras = ''.join([camera_xml_snippet(id_, ground_point) for id_, ground_point in enumerate(ground_points)])
+    out_xml = ps_cam_xml_template.format(cameras)
 
-    out_xml = ps_cam_xml_template.format(''.join([camera_xml_snippet(cam) for cam in positions]))
-    with open(to_file, 'w') as outfile:
-        outfile.write(out_xml)
+    if to_file:
+        with open(to_file, 'w') as outfile:
+            outfile.write(out_xml)
+    else:
+        return out_xml
 
 if __name__ == '__main__':
     run(dir2photoscan_cameras)
