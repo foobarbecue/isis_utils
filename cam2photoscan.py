1dea66ef7a065a91bc8cd754311c1d56fb627c54
Aaron Curtis <foobarbecue@gmail.com>
2018-02-18 17:37:25 -0800

    Switch to using lat lon (MOON2000)

--- cam2photoscan.py
+++ cam2photoscan.py
@@ -12,33 +12,40 @@ def dir2photoscan_cameras(*, from_dir, lat, lon):
     for cam_file in glob(from_dir + '*.cub'):
         try:
             res = pvl.loads(campt(from_=path.join(from_dir, cam_file), type="ground", latitude=lat, longitude=lon))
-            positions.append([cam_file, res['GroundPoint']['SpacecraftPosition']])
+            #positions.append([cam_file, res['GroundPoint']['SpacecraftPosition']])
+            positions.append([cam_file,
+                              res['GroundPoint']['SubSpacecraftLongitude'][0],
+                              res['GroundPoint']['SubSpacecraftLatitude'][0],
+                              res['GroundPoint']['SpacecraftAltitude'][0]
+                              ])
         except ProcessError as e:
             print(e.stderr)
 
     return positions
 
-def camera_xml_snippet(cam_file, position):
+def camera_xml_snippet(position):
     cam_template = """
-        <camera id="2" label="{}" sensor_id="0" enabled="1">
-        <transform>9.9911281627221693e-001 -3.1746238076266690e-002 -2.7672309780788919e-002 -8.3246669889896943e+000 -3.2885473393427074e-002 -9.9858736724610764e-001 -4.1735064588156137e-002 5.4560125134044235e-001 -2.6308287673072750e-002 4.2608054925006369e-002 -9.9874542685071577e-001 -1.4738047749381413e+000 0.0000000000000000e+000 0.0000000000000000e+000 0.0000000000000000e+000 1.0000000000000000e+000</transform>
+    <camera id="2" label="{}" sensor_id="0" enabled="1">
         <orientation>1</orientation>
-        <reference x="{x}" y="{x}" z="{z}" enabled="1"/>
-      </camera>
+        <reference x="{}" y="{}" z="{}" enabled="1"/>
+    </camera>
     """
-    return cam_template.format(file, *position)
+    return cam_template.format(*position)
 
 
-    ps_cam_xml_template = """
+ps_cam_xml_template = """
 <?xml version="1.0" encoding="UTF-8"?>
 <document version="1.3.0">
   <chunk>
     <sensors>
     </sensors>
     <cameras>
+    {}
     </cameras>
+    <reference>
+          GEOGCS["GCS_Moon_2000",DATUM["D_Moon_2000",SPHEROID["Moon_2000_IAU_IAG",1737400.0,0.0]], PRIMEM["Reference_Meridian",0.0],UNIT["Degree",0.0174532925199433]]
+    </reference>
   </chunk>
 </document>
     """
-
 
