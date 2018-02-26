e18cc4521a8802d5da1749c481b05276b3c6ee80
Aaron Curtis <foobarbecue@gmail.com>
2018-02-25 16:46:43 -0800

    allow max width cropping

--- isiscrop.py
+++ isiscrop.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 
-from pysis.isis import camrange, campt, crop
+from pysis.isis import camrange, campt, crop, catlab
 from pysis.exceptions import ProcessError
 import pvl
 from clize import run
 
-def crop_latlon(*, center_lat:float, center_lon:float, nsamples:int, nlines:int, to_cube, from_cube, pad=None,
+def crop_latlon(*, center_lat:float, center_lon:float, nsamples, nlines, to_cube, from_cube, pad=None,
                 failed_list_to=None):
     print('cropping {}'.format(from_cube))
     try:
         center_campt = pvl.loads(campt(from_= from_cube, type="ground", latitude=center_lat, longitude=center_lon))
+        catlab_data = pvl.loads(catlab(from_=from_cube))
 
     except ProcessError as e:
         print(e.stderr, e.stdout)
 
     center_pixel = (center_campt['GroundPoint']['Line'], center_campt['GroundPoint']['Sample'])
-    nw_pixel = (center_pixel[0] - nlines / 2, center_pixel[1] - nsamples / 2)
+
+    #If user asked for max width, start at left edge
+    if nsamples == 'max':
+        nw_pixel = [center_pixel[0] - int(nlines) / 2, 1]
+        nsamples = int(catlab_data['IsisCube']['Core']['Dimensions']['Samples'])
+    else:
+        nw_pixel = [center_pixel[0] - int(nlines) / 2, center_pixel[1] - int(nsamples) / 2]
+
+
     try:
+        print("sample:{} line:{}".format(nw_pixel[1], nw_pixel[0]))
         crop(from_=from_cube,
              sample=int(nw_pixel[1]),
              line=int(nw_pixel[0]),
