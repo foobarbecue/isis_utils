858e7d1dfc3f5d6a5d3934520fb2f580abdc2a52
Aaron Curtis <foobarbecue@gmail.com>
2018-02-27 17:59:27 -0800

    Add csv output

--- cam2sfm.py
+++ cam2sfm.py
@@ -9,6 +9,7 @@ from glob import glob
 import transforms3d
 import numpy
 from math import radians
+import pandas
 
 #TODO get resolution numbers from line / samples. PS ignores the cameras if resolution is wrong.
 ps_cam_xml_template = """<?xml version="1.0" encoding="UTF-8"?>
@@ -16,7 +17,7 @@ ps_cam_xml_template = """<?xml version="1.0" encoding="UTF-8"?>
   <chunk>
     <sensors>
         <sensor id="1" label="unknown" type="frame">
-        <resolution width="1000" height="1000"/>
+        <resolution width="5064" height="1000"/>
         <property name="fixed" value="0"/>
       </sensor>
     </sensors>
@@ -58,6 +59,7 @@ def cam_xml_snippet(id_, ground_point):
     cam_template_mat = """<camera id="{}" label="{}" sensor_id="1" enabled="1">
         <orientation>1</orientation>
         <transform>{}</transform>
+        <reference x="{}" y="{}" z="{}" enabled="1"/>
     </camera>
     """
     cam_template_xyzypr_ref = """<camera id="{}" label="{}" sensor_id="1" enabled="1">
@@ -78,11 +80,11 @@ def cam_xml_snippet(id_, ground_point):
     rot_mat = transforms3d.euler.euler2mat(0, yaw, radians(180) + roll, axes='rxzy')
 
     # Subtract the ground point location from the spacecraft position so we can work in small numbers
-    spacecraft_position = numpy.array(ground_point['SpacecraftPosition'].value)
-    ground_point_coords = numpy.array(ground_point['BodyFixedCoordinate'].value)
-    translation_col = (spacecraft_position - ground_point_coords).reshape(3,1)
+    spacecraft_position = numpy.array(ground_point['SpacecraftPosition'].value).reshape(3,1)
+    # ground_point_coords = numpy.array(ground_point['BodyFixedCoordinate'].value)
+    # translation_col = (spacecraft_position - ground_point_coords).reshape(3,1)
     bottom_row = numpy.array([0,0,0,1])
-    ps_transform = numpy.hstack([rot_mat, translation_col])
+    ps_transform = numpy.hstack([rot_mat, spacecraft_position])
     ps_transform = numpy.vstack([ps_transform, bottom_row])
     translation_str = ' '.join([str(x) for x in ps_transform.reshape(1,16).tolist()[0]])
 
@@ -90,8 +92,8 @@ def cam_xml_snippet(id_, ground_point):
     lat_lon_alt = [ground_point['SubSpacecraftLongitude'].value,
                    ground_point['SubSpacecraftLatitude'].value,
                    ground_point['SpacecraftAltitude'].value * 1000] # cmpt gives alt in km, Photoscan expects m
-    yaw_pitch_roll = []
-    return cam_template_mat.format(id_, label, translation_str)
+    yaw_pitch_roll = [0, 0, 0]
+    return cam_template_mat.format(id_, label, translation_str, *lat_lon_alt)
 
 def cam_xmp(ground_point):
     """
@@ -101,6 +103,21 @@ def cam_xmp(ground_point):
 
     # return xmp_cam_template.format(*ground_point['LookDirectionCamera'].value, *ground_point['SpacecraftPosition'].value)
     return xmp_cam_template_no_rot.format(*ground_point['SpacecraftPosition'].value)
+
+
+def cam_csv(ground_points, to_file):
+    """
+    :param ground_points: output from ISIS campt
+    :return: csv of camera locations
+    """
+    df = pandas.DataFrame(columns=['filename','x', 'y', 'z'], index='filename')
+    for ground_point in ground_points:
+        name = ground_point['Filename']
+        pos = ground_point['SpacecraftPosition'].value
+        df.loc[name, :] = pos
+    df.to_csv(to_file)
+    return
+
 
 def dir2sfm_cameras(*, from_dir, lat, lon, out_format=None, to_file=None, return_data=False):
     ground_points = []
@@ -118,13 +135,16 @@ def dir2sfm_cameras(*, from_dir, lat, lon, out_format=None, to_file=None, return
         except ProcessError as e:
             print(e.stderr)
 
+    if out_format=='csv':
+        cam_csv(ground_points=ground_points, to_file=to_file)
+
     if out_format=='photoscan':
         cameras = ''.join([cam_xml_snippet(id_, ground_point) for id_, ground_point in enumerate(ground_points)])
         out_xml = ps_cam_xml_template.format(cameras)
+        if to_file:
+            with open(to_file, 'w') as outfile:
+                outfile.write(out_xml)
 
-    if to_file:
-        with open(to_file, 'w') as outfile:
-            outfile.write(out_xml)
     if return_data:
         return ground_points
 
